fe743b29fde91cbfd8687d8f0ecafb1a98022050
David Fernandes <davidluisdias@gmail.com>
2017-01-18 02:40:30 +0000

    working version for not aligned cards

--- demo.py
+++ demo.py
@@ -1,67 +1,44 @@
-from __future__ import division
 import cv2
 #to show the image
-from matplotlib import pyplot as plt
 import numpy as np
 from math import cos, sin
+import os
 
 
 
 def find_biggest_contour(image):
     # Copy
     image = image.copy()
-    #input, gives all the contours, contour approximation compresses horizontal, 
-    #vertical, and diagonal segments and leaves only their end points. For example, 
-    #an up-right rectangular contour is encoded with 4 points.
-    #Optional output vector, containing information about the image topology. 
-    #It has as many elements as the number of contours.
-    #we dont need it
+   
     image, contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     # Isolate largest contour
     contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
     biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
-    print len(contours)
+    
     mask = np.zeros(image.shape, np.uint8)
     cv2.drawContours(mask, [biggest_contour], -1, (255,255,0), -1)
-
-    cv2.imshow('contour',mask)
    
-
     return biggest_contour, mask
 
-def find_suit(image):
-    #RGB stands for Red Green Blue. Most often, an RGB color is stored 
-    #in a structure or unsigned integer with Blue occupying the least 
-    #significant area (a byte in 32-bit and 24-bit formats), Green the 
-    #second least, and Red the third least. BGR is the same, except the 
-    #order of areas is reversed. Red occupies the least significant area,
-    # Green the second (still), and Blue the third.
-    # we'll be manipulating pixels directly
-    #most compatible for the transofrmations we're about to do
-    print image.shape
-    cv2.imshow('image', image);
+def find_suit_in_a_card(image, verbose=False):
+    
+    if verbose:
+        cv2.imshow('image', image);
+        cv2.imwrite('corner_used.png', image)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Make a consistent size
-    #get largest dimension
-    max_dimension = max(image.shape)
-    #The maximum window size is 700 by 660 pixels. make it fit in that
-    scale = 700/max_dimension
-    # #resize it. same width and hieght none since output is 'image'.
-    image = cv2.resize(image, None, fx=scale, fy=scale)
-    cv2.imshow('resized image', image)
-    #we want to eliminate noise from our image. clean. smooth colors without
-    #dots
+    is_red = 1
+    
     # Blurs an image using a Gaussian filter. input, kernel size, how much to filter, empty)
     image_blur = cv2.GaussianBlur(image, (7, 7), 0)
-    cv2.imshow('blured_image', image_blur)
+    if verbose:
+        cv2.imshow('corner_blured', image_blur)
+        cv2.imwrite('corner_blured.png',image_blur)
     #t unlike RGB, HSV separates luma, or the image intensity, from
     # chroma or the color information.
     #just want to focus on color, segmentation
     image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_RGB2HSV)
-
-    cv2.imshow('blured_image hsv', image_blur_hsv);
+    
     # Filter by colour
     # 0-10 hue
     #minimum red amount, max red amount
@@ -70,15 +47,20 @@ def find_suit(image):
     #layer
     mask1 = cv2.inRange(image_blur_hsv, min_red, max_red)
 
-    
-    cv2.imshow('mask1', mask1)
+    if verbose:
+        cv2.imshow('mask1_red', mask1)
+        cv2.imwrite('mask1.png',mask1)
 
     #birghtness of a color is hue
     # 170-180 hue
     min_red2 = np.array([170, 100, 80])
     max_red2 = np.array([180, 256, 256])
     mask2 = cv2.inRange(image_blur_hsv, min_red2, max_red2)
-    cv2.imshow('mask2', mask2);
+
+    if verbose:
+        cv2.imshow('mask2_red', mask2);
+        cv2.imwrite('mask2_red.png',mask2)
+
     #looking for what is in both ranges
     # Combine masks
     mask = mask1 + mask2
@@ -86,11 +68,15 @@ def find_suit(image):
 
     #Check if is not a black suit
     if np.count_nonzero(array) < 5000:
-        min_red = np.array([0, 0, 0])
-        max_red = np.array([180, 256, 50])
+        is_red = 0
+        min_black = np.array([0, 0, 0])
+        max_black = np.array([180, 256, 150])
         #layer
-        mask = cv2.inRange(image_blur_hsv, min_red, max_red)
-    cv2.imshow('result mask', mask)
+        mask = cv2.inRange(image_blur_hsv, min_black, max_black)
+
+    if verbose:
+        cv2.imshow('result mask', mask)
+        cv2.imwrite('result_mask.png',mask)
 
 
     # Clean up
@@ -101,23 +87,144 @@ def find_suit(image):
     #It is useful in closing small holes inside the foreground objects, 
     #or small black points on the object.
     mask_closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    cv2.imshow('dilation', mask_closed)
+    if verbose:
+        cv2.imshow('dilation', mask_closed)
+        cv2.imwrite('dilated_corner.png',mask_closed)
     #erosion followed by dilation. It is useful in removing noise
     mask_clean = cv2.morphologyEx(mask_closed, cv2.MORPH_OPEN, kernel)
 
-    cv2.imshow('erosion', mask_clean)
-        # Find biggest strawberry
+    if verbose:
+        cv2.imshow('erosion', mask_clean)
+        cv2.imwrite('dilated_corner_with_erosion.png',mask_clean)
+    
+    # Find biggest strawberry
     #get back list of segmented strawberries and an outline for the biggest one
     suit_contour, mask_suit = find_biggest_contour(mask_clean)
-
     
-    return suit_contour, mask_suit
+    if verbose:
+        cv2.imshow('isolated suit', mask_suit)
+        cv2.imwrite('isolated_suit.png', mask_suit)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return suit_contour, mask_suit, is_red
+
+def redSuitContours(back_corner):
+    if back_corner == 0:
+        ouros = cv2.imread('corner_ouros_up.png')
+        ouros_contour, mask_ouros, is_red = find_suit_in_a_card(ouros, False)
+
+        hearts = cv2.imread('corner_copas_up.png')
+        hearts_contour, mask_hearts, is_red = find_suit_in_a_card(hearts, False)
+    else:
+        ouros = cv2.imread('corner_ouros_back.png')
+        ouros_contour, mask_ouros, is_red = find_suit_in_a_card(ouros, False)
+
+        hearts = cv2.imread('corner_copas_back.png')
+        hearts_contour, mask_hearts, is_red = find_suit_in_a_card(hearts, False)
+
+    return ouros_contour, hearts_contour
+
+def blackSuitContours(back_corner):
+    if back_corner == 0:
+        espadas = cv2.imread('corner_espadas_up.png')
+        espadas_contour, mask_espadas, is_red = find_suit_in_a_card(espadas, False)
+
+        paus = cv2.imread('corner_paus_up.png')
+        paus_contour, mask_paus, is_red = find_suit_in_a_card(paus, True)
+    else:
+        espadas = cv2.imread('corner_espadas_back.png')
+        espadas_contour, mask_espadas, is_red = find_suit_in_a_card(espadas, False)
+
+        paus = cv2.imread('corner_paus_back.png')
+        paus_contour, mask_paus, is_red = find_suit_in_a_card(paus, False)
+
+
+    return espadas_contour, paus_contour
+
+def cornerHasRed(im):
+    image_blur = cv2.GaussianBlur(im, (7, 7), 0)
+    image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_BGR2HSV)
+    # Filter by colour
+    # 0-10 hue
+    #minimum red amount, max red amount
+    min_red = np.array([0, 100, 80])
+    max_red = np.array([10, 256, 256])
+    #layer
+    mask1 = cv2.inRange(image_blur_hsv, min_red, max_red)
+
+    #birghtness of a color is hue
+    # 170-180 hue
+    min_red2 = np.array([170, 100, 80])
+    max_red2 = np.array([180, 256, 256])
+    mask2 = cv2.inRange(image_blur_hsv, min_red2, max_red2)
+    
+    #looking for what is in both ranges
+    # Combine masks
+    mask = mask1 + mask2
+    array = np.asarray(mask)
+    print 'Numero de pixeis vermelhos: ' + str(np.count_nonzero(array))
+
+    return np.count_nonzero(array)
+
+def cornerHasBlack(im):
+    image_blur = cv2.GaussianBlur(im, (7, 7), 0)
+    image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_BGR2HSV)
+    min_black = np.array([0, 0, 0])
+    max_black = np.array([180, 256, 150])
+    #layer
+    mask = cv2.inRange(image_blur_hsv, min_black, max_black)
+    array = np.asarray(mask)
+    print 'Numero de pixeis pretos: ' + str(np.count_nonzero(array))
+
+    return np.count_nonzero(array)
+
+def compareCountours(cnt1,is_red, back_corner):
+
+    if is_red == 1:
+        ouros_contour, hearts_contour = redSuitContours(back_corner)
+        is_ouros = cv2.matchShapes(cnt1,ouros_contour,1,0.0)
+        is_hearts = cv2.matchShapes(cnt1,hearts_contour,1,0.0)
+        print 'Ouros' + str(is_ouros)
+        print 'Copas' + str(is_hearts)
+
+        return is_ouros, is_hearts
+    else:
+        espadas_contour, paus_contour = blackSuitContours(back_corner)
+        is_espadas = cv2.matchShapes(cnt1,espadas_contour,1,0.0)
+        is_paus = cv2.matchShapes(cnt1,paus_contour,1,0.0)
+
+        print 'Espadas' + str(is_espadas)
+        print 'Paus' + str(is_paus)
+
+        return is_espadas, is_paus
 
 #read the image
-image = cv2.imread('cornerkq1.png')
+#image = cv2.imread('cornerkq0.png')
 #detect it
-result = find_suit(image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-#write the new image
-#cv2.imwrite('yo2.jpg', result)
+
+def getSuitString(image, back_corner, verbose):
+    
+    suit_contour, mask_suit, is_red = find_suit_in_a_card(image, verbose)
+    
+    if is_red == 1:
+        is_ouros, is_hearts = compareCountours(suit_contour, is_red, back_corner)
+        if is_ouros > is_hearts:
+            print 'Copas'
+            return 'Copas'
+        else:
+            print 'Ouros'
+            return 'Ouros'
+    else:
+        is_espadas, is_paus = compareCountours(suit_contour, is_red, back_corner)
+        if is_espadas > is_paus:
+            print 'Paus'
+            return 'Paus'
+        else:
+            print 'Espadas'
+            return 'Espadas'
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    #write the new image
+    #cv2.imwrite('yo2.jpg', result)
--- vision.py
+++ vision.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
+import demo
 
 
 def rectify(h):
   h = h.reshape((4,2))
-  print h
   hnew = np.zeros((4,2),dtype = np.float32)
 
   add = h.sum(1)
@@ -29,15 +29,15 @@ def getCards(im, numcards=4):
   for card in contours:
     peri = cv2.arcLength(card,True)
     approx = cv2.approxPolyDP(card,0.02*peri,True)
-
-    if len(approx) == 4:
+    if (len(approx) == 4) & (cv2.contourArea(approx) > 9000):
       approx = rectify(approx)
-
-      # Show the image
-      # box = np.int0(approx)
-      # # cv2.drawContours(im,[box],0,(255,255,0),6)
-      # imx = cv2.resize(im,(1000,600))
-      # cv2.imshow('a',imx)      
+      
+      #Show the image
+      box = np.int0(approx)
+      cv2.drawContours(im,[box],0,(255,255,0),6)
+      imx = cv2.resize(im,(1000,600))
+      cv2.imshow('cards_frame',imx)
+      cv2.imwrite('cards_frame.png',imx)      
   
       h = np.array([ [0,0],[449,0],[449,449],[0,449] ],np.float32)
 
@@ -50,91 +50,88 @@ def getCards(im, numcards=4):
 
       if width > height:
         warp = cv2.transpose(warp)
-        print 'I'
+      
 
+      cv2.imshow('card',warp)
+      cv2.imwrite('card.png',warp)
   
       yield warp
 
 def getGoodCorner(img):
 
-  corner1 = img[50:150, 5:75]
+  corner1 = img[55:150, 5:75]
   corner2 = img[275:400, 5:75]
+  corner1_r = corner1.copy()
+  corner2_r = corner2.copy()
+  back_corner = 0
 
-  mean_corner1 = cv2.mean(cv2.cvtColor(corner1,cv2.COLOR_BGR2GRAY))[0]
-  cv2.imshow('corner1', corner1)
-
-  mean_corner2 = cv2.mean(cv2.cvtColor(corner2,cv2.COLOR_BGR2GRAY))[0]
-  
-  
-  print mean_corner1
-  print mean_corner2
-
-  if mean_corner1 < mean_corner2:
-    return corner1
-  else:
-    return corner2
+  corner1_red = demo.cornerHasRed(corner1_r)
+  corner2_red = demo.cornerHasRed(corner2_r)
  
-  return;
+  cv2.imshow('corner1',corner1)
+  cv2.imshow('corner2',corner2)
+
+  cv2.waitKey(0) 
+
+  if (corner1_red > 100) | (corner2_red > 100):
+    if corner1_red > corner2_red:
+    
+      return corner1, back_corner
+
+      
+    else:
+      back_corner = 1
+      return corner2, back_corner
+  
+  corner1_black = demo.cornerHasBlack(corner1_r)
+  corner2_black = demo.cornerHasBlack(corner2_r)
+
+  if (corner1_black > 100) | (corner2_black > 100):
+    if corner1_black > corner2_black:
+      return corner1, back_corner
+    else:
+      back_corner = 1
+      return corner2, back_corner
+ 
+
 
 def getInnerContours(im):
 
-  corner = getGoodCorner(im)
- 
+  corner, back_corner = getGoodCorner(im)
+  print back_corner
 
-  cv2.imshow('a', corner)
-  gray = cv2.cvtColor(corner,cv2.COLOR_BGR2GRAY)
-  blur = cv2.GaussianBlur(gray,(1,1),1000)
-  flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
+  big_corner = cv2.resize(corner,(400,400))
+  #cv2.imwrite('corner_copas_up.png',big_corner)
+  suit = demo.getSuitString(big_corner, back_corner, True)
 
-
-  image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-  contours = sorted(contours, key=cv2.contourArea,reverse=True)[:4] 
-  print len(contours)
-  #print getMeanColor(im, contours)
-#  cv2.drawContours(corner,contours ,1,(255,255,0),6)
-  imx = cv2.resize(corner,(400,400))
-  
-#  img = cv2.drawContours(im, contours, -1, (0,255,0), 3)
-
- 
-  return corner
-
-
+  return suit
 
 
 if __name__ == '__main__':
   
-  # cap = cv2.VideoCapture(1)
+  cap = cv2.VideoCapture(1)
   
-  # while(True):
-  #     # Capture frame-by-frame
-  #     ret, frame = cap.read()
+  while(True):
+    # Capture frame-by-frame
+    ret, im = cap.read()
 
-  #     # Our operations on the frame come here
-  #     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # # Our operations on the frame come herels
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-  #     # Display the resulting frame
-  #     cv2.imshow('frame',gray)
-  #     if cv2.waitKey(1) & 0xFF == ord('q'):
-  #         break
+    # # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
 
   while(True):
-    # ret, im = cap.read()
-    im = cv2.imread('kq.png')
+  #  im = cv2.imread('espadas.png')
     width = im.shape[0]
     height = im.shape[1]
 
-    # if width < height:
-    #     im = cv2.transpose(im)
-    #     im = cv2.flip(im,1)
-    cards = {}
-    for i,c in enumerate(getCards(im, 4)):
-      corner = getInnerContours(c)
-      cv2.imshow(str(i),corner)
+    for i,c in enumerate(getCards(im, 4)): 
+      cv2.imshow(getInnerContours(c),c)
       cv2.waitKey(0)
       cv2.destroyAllWindows()
-      cv2.imwrite('cornerkq'+str(i)+'.png', corner)
-      
       
        
     cv2.waitKey(0)
